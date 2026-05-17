efbe0b1b1f1fc0d57e9ee376a74f2f7a96884b46
VADYM DZIUBA <dziuba.vadym@gmail.com>
2026-05-17 20:02:07 +0300

    add url 'student_list'
    delete firstproject.students.views import

--- firstproject/students/urls.py
+++ firstproject/students/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 
-from firstproject.students.views import StudentListView, StudentCreateView
+from students.views import StudentListView, StudentCreateView
 
 urlpatterns = [
     path('', StudentListView.as_view(), name='student_list'),
